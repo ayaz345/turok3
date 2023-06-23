2e8877c81877e868667340dacb3d1ea30a9da2de
Sourcery AI <>
2023-06-23 09:54:59 +0000

    'Refactored by Sourcery'

--- versions/diff_settings.eu.py
+++ versions/diff_settings.eu.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 def apply(config, args):
-    config['mapfile'] = f'versions/eu/build/turok3.eu.map'
-    config['myimg'] = f'versions/eu/build/turok3.eu.z64'
-    config['baseimg'] = f'versions/baserom.eu.z64'
+    config['mapfile'] = 'versions/eu/build/turok3.eu.map'
+    config['myimg'] = 'versions/eu/build/turok3.eu.z64'
+    config['baseimg'] = 'versions/baserom.eu.z64'
     config['source_directories'] = ['src/eu', 'src/common', 'include', 'versions/eu/asm']
--- versions/diff_settings.us.py
+++ versions/diff_settings.us.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 def apply(config, args):
-    config['mapfile'] = f'versions/us/build/turok3.us.map'
-    config['myimg'] = f'versions/us/build/turok3.us.z64'
-    config['baseimg'] = f'versions/baserom.us.z64'
+    config['mapfile'] = 'versions/us/build/turok3.us.map'
+    config['myimg'] = 'versions/us/build/turok3.us.z64'
+    config['baseimg'] = 'versions/baserom.us.z64'
     config['source_directories'] = ['src/us', 'src/common', 'include', 'versions/us/asm']
